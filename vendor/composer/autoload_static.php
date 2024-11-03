dc7d55894a9a3f8409cc521d0a11bc1fadaa4b88
unknown <mukisapaul480@gmail.com>
2024-11-03 07:26:39 +0300

    changes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -367,8 +367,8 @@ class ComposerStaticInit6ff9bfd0370140f3de2c1b67e3241212
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -500,8 +500,8 @@ class ComposerStaticInit6ff9bfd0370140f3de2c1b67e3241212
         ),
         'Doctrine\\Common\\' => 
         array (
-            0 => __DIR__ . '/..' . '/doctrine/event-manager/src',
-            1 => __DIR__ . '/..' . '/doctrine/common/src',
+            0 => __DIR__ . '/..' . '/doctrine/common/src',
+            1 => __DIR__ . '/..' . '/doctrine/event-manager/src',
         ),
         'Dflydev\\DotAccessData\\' => 
         array (
